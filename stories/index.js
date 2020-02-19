52a5700710a654d440c7076130b5906f38199f6b
kzcheng <kevinzifancheng@gmail.com>
2020-02-19 15:52:47 -0800

    Completed Appointment Header

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import DayListItem from "components/DayListItem";
 import InterviewerList from "components/InterviewerList";
 import InterviewerListItem from "components/InterviewerListItem";
 import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
 
 const days = [
   {
@@ -135,6 +136,5 @@ storiesOf("Appointment", module)
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
   .add("Appointment", () => <Appointment />)
-  .add("Appointment with Time", () => <Appointment
-    time = "12pm"
-  />);
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm"/>);
