4410764d276afd2d39d110f7f4149b0a3dbcdd0d
kzcheng <kevinzifancheng@gmail.com>
2020-02-18 19:51:26 -0800

    Completed: W07D1 - Listing Days II

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,18 +3,29 @@ import classnames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = function(spots) {
+    if (spots === 0) {
+      return "no spots remaining";
+    } else
+      return spots === 1
+        ? `${spots} spot remaining`
+        : `${spots} spots remaining`;
+  };
+
+  console.log(props);
+
   const dayClass = classnames("day-list__item", {
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+    "day-list__item--selected": props && props.selected,
+    "day-list__item--full": props && props.spots === 0
   });
 
   return (
     <li
       className={dayClass}
-      onClick={() => props.setDay && props.setDay(props.name)}
+      onClick={() => props && props.setDay && props.setDay(props.name)}
     >
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h2 className="text--regular">{props && props.name}</h2>
+      <h3 className="text--light">{props && formatSpots(props.spots)}</h3>
     </li>
   );
 }
