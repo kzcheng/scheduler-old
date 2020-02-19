45dcaf2b510207ad098eab15e77677ff3a3a1f1a
kzcheng <kevinzifancheng@gmail.com>
2020-02-19 15:58:51 -0800

    Completed Our First Appointment Views

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import classnames from "classnames";
+import "components/Appointment/styles.scss";
+
+export default function Empty(props) {
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
+      />
+    </main>
+  );
+}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import classnames from "classnames";
 import "components/Appointment/styles.scss";
