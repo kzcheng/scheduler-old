ccd0e5dda68bf0884226535a29765f28cb4753b1
kzcheng <kevinzifancheng@gmail.com>
2020-02-19 18:59:02 -0800

    Completed Appointment Form

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 const days = [
   {
@@ -136,7 +137,7 @@ const interviewer = interviewers[0];
   ));
 }
 
-storiesOf("Appointment", module)
+{storiesOf("Appointment", module)
   .addParameters({
     backgrounds: [{ name: "white", value: "#fff", default: true }]
   })
@@ -158,4 +159,17 @@ storiesOf("Appointment", module)
   .add("Error", () => <Error
     message="Could not delete appointment."
     onClose={action("onClose")}
+  />)
+  .add("Create", () => <Form
+    interviewers={interviewers}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Edit", () => <Form
+    name="Akira"
+    interviewers={interviewers}
+    interviewer={4}
+    onSave={action("onSave")}
+    onCancel={action("onCancel")}
   />);
+}
