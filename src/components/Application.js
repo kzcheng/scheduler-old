96b57728e4d23869d6ee76917bafa9f7560818f7
kzcheng <kevinzifancheng@gmail.com>
2020-02-20 18:25:39 -0800

    Implemented Get all data

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,76 +20,34 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: [
-      {id: 1,
-        time: "11am",
-      },
-      {id: 2,
-        time: "12pm",
-        interview: {
-          student: "Lydia Miller-Jones",
-          interviewer: {
-            id: 1,
-            name: "Sylvia Palmer",
-            avatar: "https://i.imgur.com/LpaY82x.png",
-          },
-        },
-      },
-      {id: 3,
-        time: "1pm",
-      },
-      {id: 4,
-        time: "2pm",
-        interview: {
-          student: "Lydia Miller-Jones",
-          interviewer: {
-            id: 3,
-            name: "Sylvia Palmer",
-            avatar: "https://i.imgur.com/LpaY82x.png",
-          },
-        },
-      },
-      {id: 5,
-        time: "3pm",
-        interview: {
-          student: "Lydia Miller-Jones",
-          interviewer: {
-            id: 2,
-            name: "Sylvia Palmer",
-            avatar: "https://i.imgur.com/LpaY82x.png",
-          },
-        },
-      },
-      {id: "last",
-        time: "4pm",
-      },
-    ],
+    appointments: [],
+    interviewers: [],
   });
 
   const setDay = day => setState({ ...state, day });
   const setDays = days => setState({ ...state, days });
   const setAppointments = appointments => setState({ ...state, appointments });
-  
-  const getDays = axios.get("/api/days").then(res =>
-    Promise.resolve(res.data)
-  );
-  const getAppointments = axios.get("/api/appointments").then(res =>
-    Promise.resolve(res.data)
-  );
 
   // This is used to get data from the API.
   // This is a side effect.
   useEffect(() => {
     Promise.all([
-      getDays,
-      getAppointments,
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
     ]).then((res) => {
-      return setState(prev =>{
-        return { ...prev, days: res[0], appointments: res[1]};
+      setState(prev =>{
+        return {
+          ...prev,
+          days: res[0].data,
+          appointments: res[1].data,
+          interviewers: res[2].data,
+        };
       });
-    }
-    );
-  }, [getAppointments, getDays]); // This empty array will make this useEffect only happen once.
+    });
+  }, []); // This empty array will make this useEffect only happen once.
+
+
 
   const appointmentsComponent = getAppointmentsForDay(state, state.day).map(appointment => {
     return (
